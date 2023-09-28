a1295d646cd31fd2c26cb053523359f3b11e39be
namenamomiko <144974356+namenamomiko@users.noreply.github.com>
2023-09-28 12:02:21 +0900

    Update main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -8,8 +8,8 @@ function randomValueFromArray(array){
 }
 
 const storyText = '今から100年前, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+const insertX = ['猫', '犬', 'ネズミ'];
+const insertY = ['家', '学校', '塾'];
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
 randomize.addEventListener('click', result);
