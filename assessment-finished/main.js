c081bcdfb492bcfae2d7115bf7ab947830cfa34c
namenamomiko <144974356+namenamomiko@users.noreply.github.com>
2023-10-05 16:42:00 +0900

    Update main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -7,7 +7,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = '今から100年前に:insertx:は1人で歩いていました。 何も考えずに歩いていると:inserty:に到着していました。その前で少し休憩をしているとみきに会いました。突然:insertz:。みきはそれに驚いて走ってどこかに行ってしまったのでそれを追いかけました。— :insertx:は家に帰って体重計に乗ってみると、体重が50kgでした。';
+const storyText = '今から100年前に:insertx:は1人で歩いていました。 何も考えずに歩いていると:inserty:に到着していました。その前で少し休憩をしているとみきに会いました。突然:insertz:。みきはそれに驚いて走ってどこかに行ってしまったのでそれを追いかけました。— :insertx:は家に帰って体重計に乗ってみると、体重が減っていました。';
 const insertX = ['妹', 'お母さん', 'お父さん'];
 const insertY = ['家', '学校', '塾'];
 const insertZ = ['悲鳴が聞こえてきました。', '雷が鳴りました。', '怒鳴り声が聞こえてきました。'];
@@ -29,11 +29,6 @@ function result() {
   if (customName.value !== '') {
     const name = customName.value;
     newStory = newStory.replace('みき', name);
-  }
-
-  if (document.getElementById("us").checked) {
-    const 体重 = `${Math.round(50*2.2)} ポンド`;
-    newStory = newStory.replace('50kg', 体重);
   }
 
   story.textContent = newStory;
